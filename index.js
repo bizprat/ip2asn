7efcabc94a4f4212e009010a1d25f2c6555f450c
Prateek Anand <bizprat@gmail.com>
2018-10-09 15:23:09 +0530

    API created

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var IPToASN = require("ip-to-asn");
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  IPToASN = require("ip-to-asn");
 
-var client = new IPToASN();
+const client = new IPToASN();
 
-var addresses = ["139.167.218.94"];
+// var addresses = ["139.167.218.94", "8.8.8.8"];
 
-client.query(addresses, function(err, results) {
-  if (err) {
-    console.error(err);
-    return;
-  }
+app.get("/:ip", (req, res) => {
+  let addresses = req.params.ip.split(",");
+  client.query(addresses, function(err, results) {
+    if (err) {
+      // console.error(err);
+      res.send("Error");
+      return;
+    }
 
-  console.log(results);
+    // console.log(results);
+    res.send(results);
+    // res.send(req.params.ip);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
 });
