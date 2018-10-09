284daa7681778a506d3c2fa5df9fe1a6d60c4d0f
Prateek Anand <bizprat@gmail.com>
2018-10-09 13:53:27 +0530

    Test Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var IPToASN = require("ip-to-asn");
 
 var client = new IPToASN();
 
-var addresses = ["139.167.2.172"];
+var addresses = ["139.167.218.94"];
 
 client.query(addresses, function(err, results) {
   if (err) {
