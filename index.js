48d07c98217415e4f4921c7476134ce200d203bb
Prateek Anand <bizprat@gmail.com>
2018-10-15 03:47:23 +0530

    Made test.js

--- index.js
+++ index.js
@@ -8,16 +8,8 @@ const client = new IPToASN();
 // var addresses = ["139.167.218.94", "8.8.8.8"];
 
 app.get("/:ip?", (req, res) => {
-  // let ip = req.params.ip || "";
-  // let remoteIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  // // if (req.params.ip) {
-  // //   let addresses = req.params.ip.split(",");
-  // // } else {
-  // //   let addresses = req.ip;
-  // // }
-  // let addresses = ip === "" ? remoteIp : req.params.ip.split(",");
   let addresses = req.params.ip.split(",");
-  client.query(addresses, function(err, results) {
+  client.query(addresses, function (err, results) {
     if (err) {
       // console.error(err);
       res.send("Error");
---
+++ test.js
@@ -0,0 +1,25 @@
+const express = require("express"),
+    app = express(),
+    port = process.env.PORT || 3000,
+    IPToASN = require("ip-to-asn");
+
+const client = new IPToASN();
+
+var addresses = ["139.167.218.94", "8.8.8.8"];
+
+client.query(addresses, (err, results) => {
+    if (err) return err;
+    // console.log(JSON.stringify(results, null, 4))
+})
+
+app.get('/:ip', (req, res) => {
+    let addresses = req.params.ip.split(",");
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.send(req.connection.remoteAddress);
+    // console.log(req.socket.address());
+    // res.send(JSON.stringify(req, undefined, 4));
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port:", port);
+});
