0d2a2e48bd36e12d9675373d4ac383bede75d399
Prateek Anand <bizprat@gmail.com>
2018-10-15 04:00:30 +0530

    Changes to test.js

--- test.js
+++ test.js
@@ -13,9 +13,10 @@ client.query(addresses, (err, results) => {
 })
 
 app.get('/:ip?', (req, res) => {
-    let addresses1 = (req.params.ip) ? req.params.ip.split(",") : (req.headers['x-forwarded-for'] || req.connection.remoteAddress)
-    let addresses = req.params.ip.split(",");
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let addresses1 = (req.params.ip) ? req.params.ip.split(",") : ip.split(",");
+    let addresses = req.params.ip.split(",");
+
     res.send(addresses1);
     // console.log(req.socket.address());
     // res.send(JSON.stringify(req, undefined, 4));
