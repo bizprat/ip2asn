e0a6ad293c7ff3a9365ae379b8096c460db39434
Prateek Anand <bizprat@gmail.com>
2018-10-15 04:19:31 +0530

    moved test.js code to index.js

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const express = require("express"),
 
 const client = new IPToASN();
 
-// var addresses = ["139.167.218.94", "8.8.8.8"];
+var addresses = ["139.167.218.94", "8.8.8.8"];
 
-app.get("/:ip?", (req, res) => {
-  let addresses = req.params.ip.split(",");
-  client.query(addresses, function (err, results) {
-    if (err) {
-      // console.error(err);
-      res.send("Error");
-      return;
-    }
+app.get('/:ip?', (req, res) => {
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  let separateIP = ip.split(":");
+  let addresses = (req.params.ip) ? req.params.ip.split(",") : [separateIP[separateIP.length - 1]];
 
-    // console.log(results);
+  client.query(addresses, (err, results) => {
+    if (err) return err;
     res.send(results);
-    // res.send(req.ip);
   });
 });
 
